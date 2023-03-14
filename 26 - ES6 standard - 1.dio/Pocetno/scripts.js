71c2a6e0032d0ca51be20ada1518301e1cfcdb92
msostaric031 <mark.sost@gmail.com>
2023-03-14 19:30:20 +0100

    A

---
+++ 26 - ES6 standard - 1.dio/Pocetno/scripts.js
@@ -0,0 +1,12 @@
+function getX() {
+  return Math.random() >= 0.5;
+}
+var wait = new Promise((resolve, reject) => {
+  const x = getX();
+  if (x) {
+    return setTimeout(resolve, 1000);
+  }
+  return setTimeout(reject, 1000);
+})
+  .then(() => console.log(x))
+  .catch(() => console.log("Oh no!"));
