bc084b4329e5342056a3705023845d132c3494b4
Marko <54621273+msostaric-hub@users.noreply.github.com>
2023-03-20 13:36:12 +0100

    animation task

---
+++ 27 - Rad sa API-jima/Zavrsno/scripts.js
@@ -0,0 +1,9 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'https://xsmilex007.github.io/JS-API-example/mydata.json', true);
+
+request.onload = function(result) {
+	console.log(result);
+	document.write(result.currentTarget.response);
+};
+
+request.send();
